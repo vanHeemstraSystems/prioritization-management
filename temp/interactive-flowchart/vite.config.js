2db69433751c54cda6c24cd3aa06676edd2efc09
Willem van Heemstra <wvanheemstra@icloud.com>
2025-01-20 15:40:03 +0000

    Update

--- temp/interactive-flowchart/vite.config.js
+++ temp/interactive-flowchart/vite.config.js
@@ -11,5 +11,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './'
+  base: './',
+  server: {
+    allowedHosts: [
+      '.localhost',
+      '.gitpod.io'
+    ],
+    host: true
+  }
 })
